900195116a3588c507a4342506932418b375232c
AzatKabulov <kabulow16@gmail.com>
2025-07-23 22:20:03 +0800

    Added abstract class NutritionInfo
    Created abstract class NutritionInfo with basic nutrition information like carbs, fat and protein

---
+++ src/main/scala/NutritionInfo.scala
@@ -0,0 +1,8 @@
+// This is an abstract class for all nutrition-related items like Food and Meal abstract class NutritionInfo
+abstract class NutritionInfo {
+  val name: String           // Name of the food or meal
+  val calories: Double       // Total Calories
+  val protein: Double        // Protein in grams
+  val fat: Double            // Fat in grams
+  val carbs: Double          // Carbs in grams
+}
