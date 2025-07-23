9fa11a13a4cd657ce2eef4476ec7922f419fcfe1
AzatKabulov <kabulow16@gmail.com>
2025-07-23 22:37:50 +0800

    Created class Food for a single food item

---
+++ src/main/scala/Food.scala
@@ -0,0 +1,8 @@
+// Represents a single food item like "Egg" or "Apple"
+class Food (
+  val name: String,
+  val calories: Double,
+  val protein: Double,
+  val fat: Double,
+  val carbs: Double,
+)
